364d9c468780ed4cdd47da5d85eaf971484c6593
smdhar <shoumik_dhar@yahoo.com>
2025-07-14 02:37:42 -0400

    Even more responsive testing

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,8 @@ export const startButtonEl = document.createElement("button");
 startButtonEl.classList.add("button");
 startButtonEl.textContent = "START GAME";
 
-export const keyContainerEl = document.createElement("div")
+export const keyContainerEl = document.createElement("div");
+keyContainerEl.classList.add("important-keys");
 
 const firstRowLetters = "QWERTYUIOP";
 const secondRowLetters = "ASDFGHJKL";
@@ -13,19 +14,22 @@ const secondRowContainerEl = document.createElement("div");
 const thirdRowContainerEl = document.createElement("div");
 const fourthRowContainerEl = document.createElement("div");
 
+firstRowContainerEl.classList.add("first-row");
+secondRowContainerEl.classList.add("second-row");
+thirdRowContainerEl.classList.add("third-row");
+fourthRowContainerEl.classList.add("fourth-row");
+
 for (let letter of firstRowLetters) {
     const firstRowKeyEl = document.createElement("button");
     firstRowKeyEl.textContent = letter;
     firstRowKeyEl.classList.add("key");
-    firstRowKeyEl.classList.add("first-row");
     firstRowContainerEl.appendChild(firstRowKeyEl);
 }
 
 for (let letter of secondRowLetters) {
     const secondRowKeyEl = document.createElement("button");
     secondRowKeyEl.textContent = letter;
-    secondRowKeyEl.classList.add("key");
-    secondRowKeyEl.classList.add("second-row");
+    secondRowKeyEl.classList.add("key");;
     secondRowContainerEl.appendChild(secondRowKeyEl);
 }
 
@@ -33,16 +37,8 @@ for (let letter of thirdRowLetters) {
     const thirdRowKeyEl = document.createElement("button");
     thirdRowKeyEl.textContent = letter;
     thirdRowKeyEl.classList.add("key");
-    thirdRowKeyEl.classList.add("third-row");
     thirdRowContainerEl.appendChild(thirdRowKeyEl);
 }
-
-firstRowContainerEl.style.display = "flex";
-firstRowContainerEl.style.gap = "5px";
-secondRowContainerEl.style.display = "flex";
-secondRowContainerEl.style.gap = "5px";
-thirdRowContainerEl.style.display = "flex";
-thirdRowContainerEl.style.gap = "5px";
 
 export const backspaceKeyEl = document.createElement("button");
 backspaceKeyEl.classList.add("non-letter-key");
@@ -55,17 +51,9 @@ submitKeyEl.id = "submit";
 submitKeyEl.textContent = "SUBMIT";
 fourthRowContainerEl.appendChild(submitKeyEl);
 
-fourthRowContainerEl.style.display = "flex";
-fourthRowContainerEl.style.gap = "5px";
-
 [firstRowContainerEl, secondRowContainerEl, thirdRowContainerEl, fourthRowContainerEl].forEach((el) => {
     keyContainerEl.appendChild(el);
 })
-
-keyContainerEl.style.display = "flex";
-keyContainerEl.style.flexDirection = "column";
-keyContainerEl.style.alignItems = "center";
-keyContainerEl.style.gap = "5px";
 
 export const returnButtonEl = document.createElement("button");
 returnButtonEl.classList.add("button");
